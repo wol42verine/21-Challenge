a62f4ddef2bdac7b5fd60ef3300b69aa35a37c53
wol42verine <49424009+wol42verine@users.noreply.github.com>
2024-09-11 14:57:58 -0700

    Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'client', // Set the root to the client directory
   plugins: [react()],
   build: {
-    outDir: 'client/dist',  // Ensure this is where you want the build output
+    outDir: 'dist',  // Output directory relative to the root
+    rollupOptions: {
+      input: 'index.html', // Entry point for the build
+    }
   },
   server: {
     port: 3000,
