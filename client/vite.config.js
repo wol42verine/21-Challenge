a91a68233b581074f02bb2c313c542c77769aada
wol42verine <49424009+wol42verine@users.noreply.github.com>
2024-09-11 14:07:36 -0700

    Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.', // Set the root to the current directory
   plugins: [react()],
   build: {
-    outDir: 'client/dist', // Output directory relative to the root
-    rollupOptions: {
-      input: 'client/index.html', // Entry point for the build
-      external: ['@apollo/client/Link/context'] // Treat Apollo Client as external
-    }
+    outDir: 'client/dist',  // Ensure this matches where you want the build output
   },
   server: {
     port: 3000,
