f6edfe67f6bc2d966280529c32927c27e0446d45
wol42verine <49424009+wol42verine@users.noreply.github.com>
2024-09-11 14:12:00 -0700

    Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'client/dist',  // Ensure this matches where you want the build output
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['@apollo/client']  // Treat @apollo/client as external
+    }
   },
   server: {
     port: 3000,
